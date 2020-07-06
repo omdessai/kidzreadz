062aa90b60f89d8e54801f3945c1824c77f7034a
Nav Dessai <navdessai@MacBook-Air.home>
2020-07-06 09:13:12 -0400

    scanning with repetitions working + progress bar

--- src/components/scanner.js
+++ src/components/scanner.js
@@ -1,11 +1,22 @@
 /* eslint-disable no-undef */
 /* eslint-disable react-native/no-inline-styles */
 import {RNCamera} from 'react-native-camera';
-import {StyleSheet, View, TouchableOpacity, Text} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  Text,
+  TextInput,
+} from 'react-native';
 import React, {useState} from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import FeatherIcons from 'react-native-vector-icons/Feather';
+import * as Progress from 'react-native-progress';
+
+repetitionCount = 0;
+prevText = '';
+textConfirmRepetion = 2; //required to get atleast these repitions to confirm the text
 
 const scanItemTypes = {
   word: 'word',
@@ -82,6 +93,12 @@ const styles = StyleSheet.create({
   mainIconContainer: {
     position: 'absolute',
   },
+  scannedText: {
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
 });
 
 function PreviewOff({titleScanClicked, wordScanClicked}) {
@@ -150,6 +167,27 @@ function PreviewOff({titleScanClicked, wordScanClicked}) {
 
 function PreviewOn({previewStopClicked, setScanItemType, textSelected}) {
   [scanItemType, setscanItemType] = useState(setScanItemType);
+  [txtConfirmedState, settxtConfirmedState] = useState(false);
+  [confirmedText, setconfirmedText] = useState('');
+  [progressRepetionCount, setprogressRepetionCount] = useState(0);
+
+  onTextConfirmed = text => {
+    console.log(text + ' ' + prevText + ' ' + repetitionCount);
+    if (text !== prevText) {
+      setprogressRepetionCount(0);
+      setconfirmedText('');
+      prevText = text;
+
+      repetitionCount = 0;
+      return;
+    }
+    repetitionCount++;
+    setprogressRepetionCount(repetitionCount / textConfirmRepetion);
+    if (repetitionCount < textConfirmRepetion) {
+      return;
+    }
+    setconfirmedText(text); //confirmed at least required times
+  };
 
   onTextRecognized = (blocks, scanType) => {
     if (blocks.textBlocks.length > 0) {
@@ -194,7 +232,7 @@ function PreviewOn({previewStopClicked, setScanItemType, textSelected}) {
         if (wordList.length > 1) {
           tempWord = wordList[0];
           for (idx in wordList) {
-            if (idx == 0) {
+            if (idx === 0) {
               continue;
             }
             if (wordList[idx].x > wordOfInterest.x) {
@@ -216,7 +254,7 @@ function PreviewOn({previewStopClicked, setScanItemType, textSelected}) {
             w = tokens[0];
           }
           //set the selected word here:
-          textSelected(w);
+          onTextConfirmed(w);
         }
       }
       if (scanType === scanItemTypes.title) {
@@ -227,7 +265,7 @@ function PreviewOn({previewStopClicked, setScanItemType, textSelected}) {
         wordList.forEach(wordItem => {
           title += wordItem.word + ' ';
         });
-        textSelected(title.trim());
+        onTextConfirmed(title.trim());
       }
     }
   };
@@ -247,86 +285,124 @@ function PreviewOn({previewStopClicked, setScanItemType, textSelected}) {
         autoFocus={RNCamera.Constants.AutoFocus.on}>
         {({camera, status, recordAudioPermissionStatus}) => {
           return (
-            <View>
-              <View
-                style={{
-                  height: 65,
-                  flexDirection: 'row',
-                  justifyContent: 'space-around',
-                  alignItems: 'center',
-                }}>
+            <View style={{flex: 1}}>
+              <View style={{flex: 1, justifyContent: 'space-between'}}>
                 <View>
-                  <TouchableOpacity
-                    style={styles.button}
-                    onPress={() => previewStopClicked()}>
-                    <View style={styles.iconHolders}>
-                      <View style={styles.mainIconContainer}>
-                        <FeatherIcons
-                          name="camera-off"
-                          size={iconProps.mainIcon.size}
-                          color={iconProps.mainIcon.color}
-                        />
-                      </View>
+                  <View
+                    style={{
+                      height: 65,
+                      flexDirection: 'row',
+                      justifyContent: 'space-around',
+                      alignItems: 'center',
+                    }}>
+                    <View>
+                      <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => previewStopClicked()}>
+                        <View style={styles.iconHolders}>
+                          <View style={styles.mainIconContainer}>
+                            <FeatherIcons
+                              name="camera-off"
+                              size={iconProps.mainIcon.size}
+                              color={iconProps.mainIcon.color}
+                            />
+                          </View>
+                        </View>
+                      </TouchableOpacity>
                     </View>
-                  </TouchableOpacity>
+
+                    <View>
+                      <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => {
+                          setscanItemType(scanItemTypes.title);
+                        }}>
+                        <View style={styles.iconHolders}>
+                          <View style={styles.superIconContainer}>
+                            <Icon
+                              name="camera"
+                              size={iconProps.superIcon.size}
+                              color={iconProps.superIcon.color}
+                            />
+                          </View>
+                          <View style={styles.mainIconContainer}>
+                            <Icon
+                              name="book"
+                              size={iconProps.mainIcon.size}
+                              color={iconProps.mainIcon.color}
+                            />
+                          </View>
+                        </View>
+                      </TouchableOpacity>
+                    </View>
+
+                    <View>
+                      <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => {
+                          setscanItemType(scanItemTypes.word);
+                        }}>
+                        <View style={styles.iconHolders}>
+                          <View style={styles.superIconContainer}>
+                            <Icon
+                              name="camera"
+                              size={iconProps.superIcon.size}
+                              color={iconProps.superIcon.color}
+                            />
+                          </View>
+                          <View style={styles.mainIconContainer}>
+                            <Ionicons
+                              name="ios-book"
+                              size={iconProps.mainIcon.size}
+                              color={iconProps.mainIcon.color}
+                            />
+                          </View>
+                        </View>
+                      </TouchableOpacity>
+                    </View>
+                  </View>
+                  {this.scanItemType === scanItemTypes.word && (
+                    <View style={styles.wordRectangleColor} />
+                  )}
+                  {this.scanItemType === scanItemTypes.title && (
+                    <View style={styles.titleRectangleColor} />
+                  )}
                 </View>
 
-                <View>
-                  <TouchableOpacity
-                    style={styles.button}
-                    onPress={() => {
-                      setscanItemType(scanItemTypes.title);
-                    }}>
-                    <View style={styles.iconHolders}>
-                      <View style={styles.superIconContainer}>
-                        <Icon
-                          name="camera"
-                          size={iconProps.superIcon.size}
-                          color={iconProps.superIcon.color}
-                        />
-                      </View>
-                      <View style={styles.mainIconContainer}>
-                        <Icon
-                          name="book"
-                          size={iconProps.mainIcon.size}
-                          color={iconProps.mainIcon.color}
-                        />
-                      </View>
-                    </View>
-                  </TouchableOpacity>
-                </View>
-
-                <View>
-                  <TouchableOpacity
-                    style={styles.button}
-                    onPress={() => {
-                      setscanItemType(scanItemTypes.word);
-                    }}>
-                    <View style={styles.iconHolders}>
-                      <View style={styles.superIconContainer}>
-                        <Icon
-                          name="camera"
-                          size={iconProps.superIcon.size}
-                          color={iconProps.superIcon.color}
-                        />
-                      </View>
-                      <View style={styles.mainIconContainer}>
-                        <Ionicons
-                          name="ios-book"
-                          size={iconProps.mainIcon.size}
-                          color={iconProps.mainIcon.color}
-                        />
+                <View style={{height: 50, margin: 5}}>
+                  <View style={{flex: 1, marginBottom: 10}}>
+                    <Progress.Bar
+                      width={null}
+                      borderRadius={30}
+                      height={40}
+                      progress={progressRepetionCount}
+                      color="green"
+                    />
+                    <View
+                      style={{
+                        flex: 1,
+                        width: '100%',
+                        marginTop: 10,
+                        zIndex: 1,
+                        position: 'absolute',
+                        flexDirection: 'row',
+                      }}>
+                      <View style={{flex: 1, alignItems: 'center'}}>
+                        {scanItemType === scanItemTypes.word && (
+                          <Text style={styles.scannedText}>
+                            {confirmedText}
+                          </Text>
+                        )}
+                        {scanItemType === scanItemTypes.title && (
+                          <TextInput style={styles.scannedText}>
+                            {confirmedText}
+                          </TextInput>
+                        )}
                       </View>
                     </View>
-                  </TouchableOpacity>
+                  </View>
                 </View>
               </View>
-              {this.scanItemType === scanItemTypes.word && (
-                <View style={styles.wordRectangleColor} />
-              )}
-              {this.scanItemType === scanItemTypes.title && (
-                <View style={styles.titleRectangleColor} />
-              )}
             </View>
           );
         }}
@@ -345,7 +421,7 @@ export default function Scanner({onTextSelected}) {
 
   return (
     <>
-      {!this.previewVisibility && (
+      {this.previewVisibility && (
         <PreviewOff
           titleScanClicked={() => {
             setscanItemType(scanItemTypes.title);
@@ -357,7 +433,7 @@ export default function Scanner({onTextSelected}) {
           }}
         />
       )}
-      {this.previewVisibility && (
+      {!this.previewVisibility && (
         <PreviewOn
           textSelected={w => {
             onTextSelected(w);
