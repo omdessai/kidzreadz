2c70cf9ec798bd7ef935f7f79c2b53c8001ae558
Nav Dessai <navdessai@MacBook-Air.home>
2020-07-10 08:45:44 -0400

    cosmetic changes

--- src/services/store/books.js
+++ src/services/store/books.js
@@ -13,10 +13,8 @@ class Books {
   }
 
   async add(book) {
-    console.log("Adding book")
     this.bookList[book.name] = book;
     this.bookArray.push(book);
-    console.log(" book added")
   }
 
   getBook(name){
